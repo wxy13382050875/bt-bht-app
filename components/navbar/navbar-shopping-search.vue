fcda6454787c739a6b4a8995176e963f0b697896
aklili <suxy1993@gmail.com>
2019-12-21 11:14:11 +0800

    aklili 收获地址管理

---
+++ components/navbar/navbar-shopping-search.vue
@@ -0,0 +1,95 @@
+<template>
+	<view class="navbar-search-container" :style="{ height: statusHeight + navHeight + 'px' }">
+		<view class="status" :style="{ height: statusHeight + 'px' }"></view>
+		<view class="navbar-header" :style="{ height: navHeight + 'px' }">
+			<view class="nav-back" @click="backToPrev"><label class="icon iconfont aca-fanhui1"></label></view>
+			<view class="search-box">
+				<image src="/static/icon/search.png"></image>
+				<input type="text" />
+			</view>
+			<view class="list-type"><image src="/static/icon/list_type_icon.png"></image></view>
+		</view>
+	</view>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+	props: {
+		title: String
+	},
+	computed: {
+		...mapGetters({
+			curUrl: 'utp/curUrl',
+			navHeight: 'utp/navHeight',
+			statusHeight: 'utp/statusHeight'
+		})
+	},
+	methods: {
+		backToPrev() {
+			this.$Router.back();
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar-search-container {
+	position: fixed;
+	top: 0;
+	right: 0;
+	left: 0;
+	z-index: 1993;
+	background: $nav-bg-color;
+
+	.navbar-header {
+		position: relative;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		.nav-back {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			margin-right: 28rpx;
+			width: 62rpx;
+			height: 100%;
+			color: $text-color-white;
+			font-size: 34rpx;
+			z-index: 22;
+
+			.icon {
+				font-size: 34rpx;
+			}
+		}
+		.search-box {
+			flex: 1;
+			display: flex;
+			align-items: center;
+			height: 58rpx;
+			background: rgba(255, 255, 255, 1);
+			border-radius: 28px;
+			image {
+				margin-left: 17rpx;
+				width: 37rpx;
+				height: 37rpx;
+			}
+			input{
+				margin-left: 15rpx;
+				flex: 1;
+				height: 100%;
+			}
+		}
+		.list-type {
+			margin-left: 28rpx;
+			margin-right: 22rpx;
+			width: 41rpx;
+			height: 41rpx;
+			image {
+				height: 100%;
+				width: 100%;
+			}
+		}
+	}
+}
+</style>
---
+++ pages/user/my-address-create.vue
@@ -0,0 +1,16 @@
+<template>
+	<view class="my-address-create-container"><bht-layout-container></bht-layout-container></view>
+</template>
+
+<script>
+/**
+ * 地址新增/更新
+ */
+export default{
+	
+}
+</script>
+
+<style lang="scss">
+	
+</style>
