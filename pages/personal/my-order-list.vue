4e76af509f0347723f32188d798631f425fe12f4
武新义 <ly-080208@163.com>
2020-01-06 15:49:34 +0800

    相等改我的订单跳转

--- pages/personal/my-order-list.vue
+++ pages/personal/my-order-list.vue
@@ -4,27 +4,27 @@
 		<bht-layout-container :bottom="0">
 			<!-- <scroll-view class="nav-area" scroll-x :scroll-left="navScrollLeft"> -->
 			<!-- 菜单 -->
-		<view class="top-warp">
-			<!-- <view class="tip">每个菜单列表仅初始化一次,切换菜单缓存数据</view> -->
-			<view class="nav">
-				<view v-for="(tab, i) in tabs" :key="i" :class="{'active':curIndex===i}" @click="changeTab(i)">{{tab}}</view>
+			<view class="top-warp">
+				<!-- <view class="tip">每个菜单列表仅初始化一次,切换菜单缓存数据</view> -->
+				<view class="nav">
+					<view v-for="(tab, i) in tabs" :key="i" :class="{ active: curIndex === i }" @click="changeTab(i)">{{ tab }}</view>
+				</view>
 			</view>
-		</view>
-		
-		<!--全部 -->
-		<orderlist :i="0" :index="curIndex"></orderlist>
-		
-		<!-- 奶粉 -->
-		<orderlist :i="1" :index="curIndex"></orderlist>
-		
-		<!-- 面膜 -->
-		<orderlist :i="2" :index="curIndex"></orderlist>
-		
-		<!-- 图书 -->
-		<orderlist :i="3" :index="curIndex"></orderlist>
-		
-		<!-- 图书 -->
-		<orderlist :i="4" :index="curIndex"></orderlist>
+
+			<!--全部 -->
+			<orderlist :i="0" :index="curIndex"></orderlist>
+
+			<!-- 奶粉 -->
+			<orderlist :i="1" :index="curIndex"></orderlist>
+
+			<!-- 面膜 -->
+			<orderlist :i="2" :index="curIndex"></orderlist>
+
+			<!-- 图书 -->
+			<orderlist :i="3" :index="curIndex"></orderlist>
+
+			<!-- 图书 -->
+			<orderlist :i="4" :index="curIndex"></orderlist>
 			<!-- <view class="nav-area">
 				<view class="scroller" :style="{ width: navItemWidth * navList.length + 'px' }">
 					<view class="scroller-wrapper">
@@ -52,58 +52,61 @@
 </template>
 
 <script>
-	import orderlist from "@/components/personal/xw-dth-order-list.vue";
-	
-	export default {
-		components: {
-			orderlist
-		},
-		data() {
-			return {
-				tabs: ['全部', '待付款', '待发货', '已收货','已完成'],
-				curIndex: 0 // 当前tab的下标
-			}
-		},
-		methods: {
-			// 切换菜单
-			changeTab (i) {
-				this.curIndex = i
-			}
-		}
-	}
+import orderlist from '@/components/personal/xw-dth-order-list.vue';
 
+export default {
+	components: {
+		orderlist
+	},
+	data() {
+		return {
+			tabs: ['全部', '待付款', '待发货', '已收货', '已完成'],
+			curIndex: 0 // 当前tab的下标
+		};
+	},
+	methods: {
+		// 切换菜单
+		changeTab(i) {
+			this.curIndex = i;
+		}
+	},
+	onLoad: function(option) {
+		setTimeout(() => {
+			this.curIndex = Number(option.currentIndex);
+		}, 10);
+	}
+};
 </script>
 
 <style lang="scss" scoped>
-	.top-warp{
-		z-index: 9990;
-		position: fixed;
-		top: --window-top; /* css变量 */
-		left: 0;
-		width: 100%;
-		height: 80rpx;
-		background-color: white;
-	}
-	.top-warp .tip{
-		font-size: 28upx;
-		height: 80rpx;
-		line-height: 80rpx;
-		text-align: center;
-	}
-	.top-warp .nav{
-		text-align: center;
-		height: 80rpx;
-		border-bottom: 1upx solid #ddd;
-	}
-	.top-warp .nav view{
-		display: inline-block;
-		width: 20%;
-		line-height: 80rpx;
-		font-size: 28upx;
-	}
-	.top-warp .nav .active{
-		border-bottom: 2upx solid #FF6990;
-		color: #FF6990;
-	}
-
+.top-warp {
+	z-index: 9990;
+	position: fixed;
+	top: --window-top; /* css变量 */
+	left: 0;
+	width: 100%;
+	height: 80rpx;
+	background-color: white;
+}
+.top-warp .tip {
+	font-size: 28upx;
+	height: 80rpx;
+	line-height: 80rpx;
+	text-align: center;
+}
+.top-warp .nav {
+	text-align: center;
+	height: 80rpx;
+	border-bottom: 1upx solid #ddd;
+}
+.top-warp .nav view {
+	display: inline-block;
+	width: 20%;
+	line-height: 80rpx;
+	font-size: 28upx;
+}
+.top-warp .nav .active {
+	border-bottom: 2upx solid #ff6990;
+	color: #ff6990;
+}
 </style>
