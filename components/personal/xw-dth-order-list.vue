53422ef9d5495eb6a2644660fc917ece3157e9a8
武新义 <ly-080208@163.com>
2020-01-06 15:30:07 +0800

    修改我的订单列表，多页面下拉刷新问题

--- components/personal/xw-dth-order-list.vue
+++ components/personal/xw-dth-order-list.vue
@@ -1,8 +1,13 @@
 <template>
-	<!-- <orderList></orderList> -->
-	<mescroll-uni class="mescroll" @init="initMescroll" :down="downOption" :up="upOption" @up="upCallback" @down="downCallback" :fixed="false">
-		<view v-for="(item, eIndex) in dataSource" :key="eIndex"><ordercell :dataSource="item"></ordercell></view>
-	</mescroll-uni>
+	<!-- 不能用v-if -->
+	<view v-show="i === index">
+		<!-- top="xxx"下拉布局往下偏移,防止被悬浮菜单遮住 -->
+		<mescroll-uni  top="160" :down="downOption" @down="downCallback" :up="upOption" @up="upCallback" @emptyclick="emptyClick" @init="mescrollInit">
+			<!-- 数据列表 -->
+			<!-- <ordercell :dataSource="dataSource"></ordercell> -->
+			<view v-for="(item, eIndex) in dataSource" :key="eIndex"><ordercell :dataSource="item"></ordercell></view>
+		</mescroll-uni>
+	</view>
 </template>
 
 <script>
@@ -13,65 +18,140 @@ export default {
 	components: {
 		ordercell
 	},
+	props: {
+		statusCd: String
+	},
 	data() {
 		return {
-			lists: [],
-			dataSource: [],
+			mescroll: null, //mescroll实例对象
 			downOption: {
-				autoShowLoading: true,
-				textInOffset: '下拉即可刷新...',
-				textOutOffset: '松开即可刷新...',
-				textLoading: '努力加载中...'
+				auto: false // 不自动加载
 			},
 			upOption: {
-				auto: true,
-				noMoreSize: 5,
+				auto: false, // 不自动加载
+				// page: {
+				// 	num: 0, // 当前页码,默认0,回调之前会加1,即callback(page)会从1开始
+				// 	size: 10 // 每页数据的数量
+				// },
+				noMoreSize: 4, //如果列表已无数据,可设置列表的总数量要大于半页才显示无更多数据;避免列表数据过少(比如只有一条数据),显示无更多数据会不好看; 默认5
 				empty: {
-					tip: '没有查询到数据',
-					icon: ''
-				},
-				textNoMore: '没有更多数据了'
+					tip: '~ 空空如也 ~', // 提示
+					btnText: '去看看'
+				}
 			},
-
-			params: {},
-			statusCd: 0
+			dataSource: [], //列表数据
+			isInit: false, // 列表是否已经初始化
+			scrollY: 0,
+			params: {}
 		};
 	},
+	props: {
+		i: Number, // 每个tab页的专属下标
+		index: {
+			// 当前tab的下标
+			type: Number,
+			default() {
+				return 0;
+			}
+		}
+	},
+	watch: {
+		// 监听下标的变化
+		index(val) {
+			if (this.i === val && !this.isInit) {
+				this.isInit = true; // 标记为true
+				this.mescroll.triggerDownScroll();
+			}
+		}
+	},
+	mounted() {
+		// 第一个tab,自动加载数据
+		if (this.i === 0) {
+			this.isInit = true; // 标记为true
+			this.mescroll.triggerDownScroll();
+		}
+	},
 	methods: {
-		//获取mescroll对象
-		initMescroll(mescroll) {
+		// mescroll组件初始化的回调,可获取到mescroll对象
+		mescrollInit(mescroll) {
 			this.mescroll = mescroll;
 		},
-		//下拉刷新
+		/*下拉刷新的回调 */
 		downCallback(mescroll) {
-			console.log('downCallback');
-			mescroll.num = 1;
+			// 这里加载你想下拉刷新的数据, 比如刷新轮播数据
+			// loadSwiper();
+			// 下拉刷新的回调,默认重置上拉加载列表为第一页 (自动执行 mescroll.num=1, 再触发upCallback方法 )
 			mescroll.resetUpScroll();
 		},
-		//上拉刷新
+		/*上拉加载的回调: mescroll携带page的参数, 其中num:当前页 从1开始, size:每页数据条数,默认10 */
 		upCallback(mescroll) {
-			console.log('upCallback');
+			//联网加载数据
+			this.getListDataFromNet(
+				mescroll.num,
+				mescroll.size,
+				curPageData => {
+					//联网成功的回调,隐藏下拉刷新和上拉加载的状态;
+					console.log('i=' + this.i + ', mescroll.num=' + mescroll.num + ', mescroll.size=' + mescroll.size + ', curPageData.length=' + curPageData.length);
+					mescroll.endSuccess(curPageData.length);
+					//设置列表数据
+					if (mescroll.num == 1) this.dataSource = []; //如果是第一页需手动制空列表
+					this.dataSource = this.dataSource.concat(curPageData); //追加新数据
+				},
+				() => {
+					//联网失败的回调,隐藏下拉刷新的状态
+					mescroll.endErr();
+				}
+			);
+		},
+		//点击空布局按钮的回调
+		emptyClick() {
+			// uni.showToast({
+			// 	title: '点击了按钮,具体逻辑自行实现'
+			// });
+			uni.navigateBack();
+		},
 
-			this.params.pageIndex = mescroll.num;
-			this.params.pageSize = mescroll.size;
-			this.params.statusCd = this.statusCd;
+		/*联网加载列表数据
+			在您的实际项目中,请参考官方写法: http://www.mescroll.com/uni.html#tagUpCallback
+			请忽略getListDataFromNet的逻辑,这里仅仅是在本地模拟分页数据,本地演示用
+			实际项目以您服务器接口返回的数据为准,无需本地处理分页.
+			* */
+		getListDataFromNet(pageNum, pageSize, successCallback, errorCallback) {
+			console.log('upCallback');
 			let userInfo = uni.getStorageSync('userInfo');
+			this.params.pageIndex = pageNum;
+			this.params.pageSize = pageSize;
+			if (this.i === 0) {
+				this.params.statusCd = 0;
+			} else if (this.i === 1) {
+				this.params.statusCd = 10008;
+			} else if (this.i === 2) {
+				this.params.statusCd = 10016;
+			} else if (this.i === 3) {
+				this.params.statusCd = 10020;
+			} else if (this.i === 4) {
+				this.params.statusCd = 10024;
+			}
 
 			this.params.shopCarNbr = userInfo.shopCarNbr;
 
 			console.log(this.params);
 			getOrderList(this.params).then(res => {
 				let { pageInfo, stores } = res.data;
-
-				console.log(stores);
-				if (mescroll.num == 1) this.dataSource = [];
-				this.dataSource = this.dataSource.concat(stores);
-				mescroll.endBySize(stores.length, pageInfo.rowCount);
-				this.$nextTick(() => {
-					mescroll.endSuccess(stores.length);
-				});
+				
+				if(res.code =='200'){
+					let listData = []
+					listData = stores;
+					successCallback && successCallback(listData);
+				} else {
+					errorCallback && errorCallback();
+				}
+				
 			});
+			
 		}
+
+		
 	}
 };
 </script>
--- pages/personal/my-order-list.vue
+++ pages/personal/my-order-list.vue
@@ -3,7 +3,29 @@
 		<nav-bar-back title="我的订单" popType="0"></nav-bar-back>
 		<bht-layout-container :bottom="0">
 			<!-- <scroll-view class="nav-area" scroll-x :scroll-left="navScrollLeft"> -->
-			<view class="nav-area">
+			<!-- 菜单 -->
+		<view class="top-warp">
+			<!-- <view class="tip">每个菜单列表仅初始化一次,切换菜单缓存数据</view> -->
+			<view class="nav">
+				<view v-for="(tab, i) in tabs" :key="i" :class="{'active':curIndex===i}" @click="changeTab(i)">{{tab}}</view>
+			</view>
+		</view>
+		
+		<!--全部 -->
+		<orderlist :i="0" :index="curIndex"></orderlist>
+		
+		<!-- 奶粉 -->
+		<orderlist :i="1" :index="curIndex"></orderlist>
+		
+		<!-- 面膜 -->
+		<orderlist :i="2" :index="curIndex"></orderlist>
+		
+		<!-- 图书 -->
+		<orderlist :i="3" :index="curIndex"></orderlist>
+		
+		<!-- 图书 -->
+		<orderlist :i="4" :index="curIndex"></orderlist>
+			<!-- <view class="nav-area">
 				<view class="scroller" :style="{ width: navItemWidth * navList.length + 'px' }">
 					<view class="scroller-wrapper">
 						<view
@@ -19,220 +41,69 @@
 					</view>
 				</view>
 			</view>
-			<!-- </scroll-view> -->
 			<view class="swiper-area">
-				<!-- <pulldown-refresher ref="pulldownRefresher" @pulldownRefresh="refresh"> -->
 				<mescroll-uni class="mescroll" @init="initMescroll" :down="downOption" :up="upOption" @up="upCallback" @down="downCallback" :fixed="false">
-					<!-- <swiper :current="switchIndex" style="height: 100%;" :duration="567" @transition="transition" @change="change" @animationfinish="animationfinish">
-						<swiper-item v-for="(item, pIndex) in navList" :key="pIndex" style="height: 100%;">
-							<view v-for="(item, eIndex) in dataSource" style="height: 100%;" :key="eIndex"><ordercell :dataSource="item"></ordercell></view>
-						</swiper-item>
-					</swiper> -->
+
 					<view v-for="(item, eIndex) in dataSource" :key="eIndex"><ordercell :dataSource="item"></ordercell></view>
 				</mescroll-uni>
-				<!-- </pulldown-refresher> -->
-			</view>
+			</view> -->
 		</bht-layout-container>
 	</view>
 </template>
 
 <script>
-import pulldownRefresher from '@/third/pullsownrefresh/pulldown-refresher.vue';
-import ordercell from '@/components/personal/xw-dth-order-cell.vue';
-import { getOrderList } from '@/api/shop.js';
-export default {
-	components: {
-		pulldownRefresher,
-		ordercell
-	},
-	data() {
-		return {
-			lists: [],
-			dataSource: [],
-			switchIndex: 0, // 使 swiper 切换到的 index
-			changeIndex: 0, // swiper change 时的 index
-			finishedIndex: 0, // swiper 停止滑动后的 index
-			screenWidth: uni.getSystemInfoSync().screenWidth, // 屏幕宽
-			navScrollLeft: 0, // 导航滚动区的滚动距离
-			activeBarLeft: 0, // 滑块距离左边距离swiper-area
-			navItemWidth: uni.getSystemInfoSync().screenWidth / 5, // navitem 宽
-			// pulldownRefresh: 0, // 下拉刷新容器距离顶部的距离
-			// scrollViewHeight: 0, // scroll view 高
-			// pulldownRefreshing: false, // 页面是否正处于下拉刷新状态
-			// pullDownStopDistance: 0, //手指松开时下拉刷新容器距离顶部的距离
-			// pulldownRefreshingTop: 0, //手指松开后下拉刷新容器距离顶部的距离
-			// pullDownRefreshDistance: 80, // 下拉刷新距离
-
-			downOption: {
-				autoShowLoading: true,
-				textInOffset: '下拉即可刷新...',
-				textOutOffset: '松开即可刷新...',
-				textLoading: '努力加载中...'
-			},
-			upOption: {
-				auto: true,
-				noMoreSize: 5,
-				empty: {
-					tip: '没有查询到数据',
-					icon: ''
-				},
-				textNoMore: '没有更多数据了'
-			},
-			navList: [
-				{ name: '全部', value: '0' },
-				{ name: '待付款', value: '10008' },
-				{ name: '待发货', value: '10016' },
-				{ name: '待收货', value: '10020' },
-				{ name: '已完成', value: '10024' }
-			],
-			params: {},
-			statusCd: 0
-		};
-	},
-	onLoad: function(option) {
-		this.statusCd = this.navList[option.currentIndex].value;
-		this.changeIndex = option.currentIndex;
-	},
-	created() {
-		// 乱序
-		// this.navList.forEach(() => this.datsSource.push(JSON.parse(JSON.stringify(this.datsSource.sort(() => Math.random() - 0.5)))))
-	},
-	mounted() {
-		// 组件挂载后设置 scrollview 高
-		// setTimeout(()=>{
-		// 	this.$offset('.swiper-area').then(res => {
-		// 		this.scrollViewHeight = res.height
-		// 		console.log(res)
-		// 	})
-		// },100)
-	},
-	methods: {
-		tapNav(index) {
-			console.log('tapNav');
-			// 点击 bavbar 切换
-			this.switchIndex = index;
-			this.changeIndex = index;
-			this.statusCd = this.navList[index].value;
-			this.dataSource = [];
-			this.mescroll.triggerUpScroll();
+	import orderlist from "@/components/personal/xw-dth-order-list.vue";
+	
+	export default {
+		components: {
+			orderlist
 		},
-		$offset(selector) {
-			// 获取组件内元素的 offset 信息
-			return new Promise(resolve =>
-				uni
-					.createSelectorQuery()
-					.in(this)
-					.select(selector)
-					.boundingClientRect(data => resolve(data))
-					.exec()
-			);
+		data() {
+			return {
+				tabs: ['全部', '待付款', '待发货', '已收货','已完成'],
+				curIndex: 0 // 当前tab的下标
+			}
 		},
-
-		//获取mescroll对象
-		initMescroll(mescroll) {
-			this.mescroll = mescroll;
-		},
-		//下拉刷新
-		downCallback(mescroll) {
-			console.log('downCallback');
-			mescroll.resetUpScroll();
-		},
-		//上拉刷新
-		upCallback(mescroll) {
-			console.log('upCallback');
-			
-			this.params.pageIndex = mescroll.num;
-			this.params.pageSize = mescroll.size;
-			this.params.statusCd = this.statusCd;
-			let userInfo = uni.getStorageSync('userInfo');
-
-			this.params.shopCarNbr = userInfo.shopCarNbr;
-
-			console.log(this.params);
-			getOrderList(this.params).then(res => {
-				let { pageInfo, stores } = res.data;
-
-				if (mescroll.num == 1) this.dataSource = [];
-				this.dataSource = this.dataSource.concat(stores);
-				mescroll.endBySize(stores.length, pageInfo.rowCount);
-				this.$nextTick(() => {
-					mescroll.endSuccess(stores.length);
-				});
-			});
+		methods: {
+			// 切换菜单
+			changeTab (i) {
+				this.curIndex = i
+			}
 		}
 	}
-};
+
 </script>
 
 <style lang="scss" scoped>
-@mixin flex {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-}
-.content {
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	.status_bar {
-		width: 100%;
-		background: #007aff;
-		height: var(--status-bar-height);
-	}
-	.pulldownrefresh {
-		left: 50%;
-		bottom: 0;
-		z-index: 1;
+	.top-warp{
+		z-index: 9990;
 		position: fixed;
-		transform: translateX(-50%);
-	}
-	.nav-area {
+		top: --window-top; /* css变量 */
+		left: 0;
 		width: 100%;
-		height: 100rpx;
-		.scroller {
-			height: 100%;
-			background: #ffffff;
-			.scroller-wrapper {
-				height: 100%;
-				.scroller-item {
-					@include flex();
-					float: left;
-					height: 100%;
-					&.active {
-						position: relative;
-						color: #ff3333;
-					}
-					&.active::after {
-						content: '';
-						position: absolute;
-						right: 0;
-						bottom: 0;
-						left: 0;
-						height: 2px;
-						background: #ff3333;
-					}
-				}
-			}
-		}
+		height: 80rpx;
+		background-color: white;
 	}
-	.swiper-area {
-		flex: 1;
-		width: 100%;
-		height: 100%;
+	.top-warp .tip{
+		font-size: 28upx;
+		height: 80rpx;
+		line-height: 80rpx;
+		text-align: center;
+	}
+	.top-warp .nav{
+		text-align: center;
+		height: 80rpx;
+		border-bottom: 1upx solid #ddd;
+	}
+	.top-warp .nav view{
+		display: inline-block;
+		width: 20%;
+		line-height: 80rpx;
+		font-size: 28upx;
+	}
+	.top-warp .nav .active{
+		border-bottom: 2upx solid #FF6990;
+		color: #FF6990;
+	}
 
-		swiper {
-			height: 100%;
-			background: #f8f8f8;
-			swiper-item {
-				image {
-					float: left;
-					width: 47vw;
-					height: 47vw;
-					border-radius: 1vw;
-					margin: 2vw 0 0 2vw;
-				}
-			}
-		}
-	}
-}
 </style>
