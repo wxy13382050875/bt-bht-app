a0575074254fa11ba73cd2057c318d2e1af06fbe
武新义 <ly-080208@163.com>
2020-01-07 18:06:21 +0800

    wx

---
+++ data/menu.js
@@ -0,0 +1,54 @@
+const menuData = {
+	"1": [{
+			title: '首页',
+			src: require('@/static/tab/home_def_icon.png'),
+			selectedSrc: require('@/static/tab/home_sel_icon.png'),
+			flag: 'index'
+		},
+		{
+			title: '统计报表',
+			src: require('@/static/tab/tjbb_def_icon.png'),
+			selectedSrc: require('@/static/tab/tjbb_sel_icon.png'),
+			flag: 'stat'
+		},
+		{
+			title: '购物车',
+			src: require('@/static/tab/cart_def_icon.png'),
+			selectedSrc: require('@/static/tab/cart_sel_icon.png'),
+			flag: 'cart'
+		},
+		{
+			title: '我的',
+			src: require('@/static/tab/my_def_icon.png'),
+			selectedSrc: require('@/static/tab/my_sel_icon.png'),
+			flag: 'my'
+		}
+	],
+	"2": [{
+			title: '首页',
+			src: require('@/static/tab/home_def_icon.png'),
+			selectedSrc: require('@/static/tab/home_sel_icon.png'),
+			flag: 'index'
+		},
+		{
+			title: '统计报表',
+			src: require('@/static/tab/tjbb_def_icon.png'),
+			selectedSrc: require('@/static/tab/tjbb_sel_icon.png'),
+			flag: 'stat'
+		},
+		{
+			title: '购物车',
+			src: require('@/static/tab/cart_def_icon.png'),
+			selectedSrc: require('@/static/tab/cart_sel_icon.png'),
+			flag: 'cart'
+		},
+		{
+			title: '我的',
+			src: require('@/static/tab/my_def_icon.png'),
+			selectedSrc: require('@/static/tab/my_sel_icon.png'),
+			flag: 'my'
+		}
+	]
+}
+
+export default menuData;
